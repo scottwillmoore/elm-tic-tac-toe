feee6f168150faee179672ef0e6471fb3ca7afc1
Scott Moore <scott.will.moore@gmail.com>
2018-02-05 22:54:17 +1100

    setup elm html.program boilerplate

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,47 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (..)
+import Html.Events exposing (..)
 
+
+main : Program Never Model Msg
 main =
-    text "Hello, world!"
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( 0, Cmd.none )
+
+
+type alias Model =
+    Int
+
+
+type Msg
+    = Increment
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Increment ->
+            ( model + 1, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ toString model |> text
+        , button [ onClick Increment ] [ text "Increment" ]
+        ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
