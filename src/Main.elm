fe83d2dae238e9c074754b8d2cd08e38cecc92fa
Scott Moore <scott.will.moore@gmail.com>
2018-03-03 00:12:39 +1100

    refractor types and functions

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
+import Array exposing (Array)
 import Dict exposing (Dict)
 
 
@@ -25,25 +26,50 @@ type alias Position =
     ( Int, Int )
 
 
-positions : List Position
-positions =
-    [ ( -1, -1 ), ( 0, -1 ), ( 1, -1 ), ( -1, 0 ), ( 0, 0 ), ( 1, 0 ), ( -1, 1 ), ( 0, 1 ), ( 1, 1 ) ]
+type alias Cell =
+    Maybe Player
 
 
 type alias Board =
-    { turn : Player
-    , tiles : Dict Position Player
-    }
+    Dict Position Player
+
+
+product : List a -> List a -> List (List a)
+product xs ys =
+    case xs of
+        [] ->
+            []
+
+        z :: zs ->
+            List.foldr (\a b -> [ a, z ] :: b) [] ys ++ product zs ys
+
+
+tuple2 : ( a, a ) -> List a -> ( a, a )
+tuple2 default list =
+    case list of
+        [ a, b ] ->
+            ( a, b )
+
+        _ ->
+            default
+
+
+positions : List Position
+positions =
+    product [ 1, 2, 3 ] [ 1, 2, 3 ]
+        |> List.map (tuple2 ( 0, 0 ))
 
 
 type alias Model =
-    Board
+    { turn : Player
+    , board : Board
+    }
 
 
 defaultModel : Model
 defaultModel =
     { turn = O
-    , tiles = Dict.empty
+    , board = Dict.empty
     }
 
 
@@ -54,8 +80,47 @@ init =
 
 type Msg
     = Reset
-    | Play Position
-    | Fill Position Player
+    | CellClicked Position
+
+
+
+-- CellClicked
+-- |> attemptPlacement
+-- |> swapTurn
+-- |> checkBoard
+-- |>
+
+
+swapPlayer : Player -> Player
+swapPlayer player =
+    case player of
+        X ->
+            O
+
+        O ->
+            X
+
+
+attemptPlacement : Model -> Board -> Player -> Position -> Model
+attemptPlacement model board player position =
+    let
+        cell =
+            Dict.get position board
+    in
+        case cell of
+            Nothing ->
+                { model
+                    | turn = swapPlayer player
+                    , board = Dict.insert position player board
+                }
+
+            Just player ->
+                model
+
+
+checkStatus : Board -> Bool
+checkStatus board =
+    False
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -64,45 +129,49 @@ update msg model =
         Reset ->
             ( defaultModel, Cmd.none )
 
-        Play position ->
+        CellClicked position ->
             let
-                nextTurn =
-                    case model.turn of
-                        X ->
-                            O
+                nextModel =
+                    attemptPlacement model model.board model.turn position
 
-                        O ->
-                            X
+                reset =
+                    checkStatus nextModel.board
             in
-                if position == (1, 1) then
-                    update Reset model
+                if reset then
+                    ( defaultModel, Cmd.none )
                 else
-                    update (Fill position nextTurn) { model | turn = nextTurn }
-
-        Fill position player ->
-            ( { model | tiles = Dict.insert position player model.tiles }, Cmd.none )
+                    ( nextModel, Cmd.none )
 
 
-toDom : Dict Position Player -> Position -> Html Msg
-toDom tiles position =
-    let
-        tile =
-            Dict.get position tiles
-    in
-        case tile of
-            Nothing ->
-                div [ class "tile", onClick (Play position) ] []
+viewCell : Position -> Cell -> Html Msg
+viewCell position cell =
+    div [ class "cell", onClick (CellClicked position) ]
+        [ text
+            (cell
+                |> Maybe.map toString
+                |> Maybe.withDefault ""
+            )
+        ]
 
-            Just player ->
-                div [ class "tile" ] [ text (toString player) ]
+
+viewBoard : Board -> Html Msg
+viewBoard board =
+    div [ class "board" ]
+        (positions
+            |> List.map
+                (\position ->
+                    board
+                        |> Dict.get position
+                        |> viewCell position
+                )
+        )
 
 
 view : Model -> Html Msg
 view model =
     div [ class "app" ]
         [ h1 [ onClick Reset ] [ text "Tic-Tac-Toe" ]
-        , div [ class "board" ]
-            (List.map (toDom model.tiles) positions)
+        , viewBoard model.board
         ]
 
 
